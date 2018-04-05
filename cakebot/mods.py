49f1da624ddd0b75a195d0f1d79fb4445d5b85e0
Jeffrey Seifried <jeffseif@yelp.com>
2018-04-05 13:30:41 -0700

    Added zero-width space to message forwarding so that users are not pinged

--- cakebot/mods.py
+++ cakebot/mods.py
@@ -3,6 +3,9 @@ from cakebot import KILL_SWITCH
 from cakebot.bind import bind
 
 
+ZERO_WIDTH_SPACE = '\u200b'
+
+
 class DeathException(Exception):
     pass
 
@@ -23,11 +26,15 @@ def echo(self, conn, event, message, match):
     self.send(conn, event, reply)
 
 
+def pingless_nick(nick):
+    return nick[0] + ZERO_WIDTH_SPACE + nick[1:]
+
+
 def forward(self, conn, event, message, match):
     if event.target in self.listens:
         prefix = '.'.join((
             event.target,
-            event.source.nick,
+            pingless_nick(event.source.nick),
         ))
         message = '{prefix}: `{message}`'.format(prefix=prefix, message=message)
         for channel in self.forwards:
